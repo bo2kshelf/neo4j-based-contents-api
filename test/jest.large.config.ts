f1c1e10f2a8618f00a029b4713f1156b207e69f4
SnO2WMaN <me@sno2wman.dev>
2021-02-21 23:15:51 +0900

    test: neo4jが絡むテストでjest timeoutを30秒まで延長

--- test/jest.large.config.ts
+++ test/jest.large.config.ts
@@ -3,6 +3,7 @@ import base from './jest.base.config';
 
 const config: Config.InitialOptions = {
   ...base,
+  testTimeout: 30000,
   collectCoverage: true,
   coverageDirectory: '../coverage/large',
   reporters: ['default', ['jest-junit', {outputDirectory: 'coverage/large'}]],
